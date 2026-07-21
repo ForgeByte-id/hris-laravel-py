1e043df4dcada78ab709bd307d4584d9715aa1f9
novasugiantara <novasugiantara425@gmail.com>
2026-07-21 18:48:57 +0800

    fix: update middleware to trust proxies and adjust entrypoint script for view caching

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +13,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache || true
+php artisan view:cache
+php artisan optimize:clear || true
 
 exec "$@"
